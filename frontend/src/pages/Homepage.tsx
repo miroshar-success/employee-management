ae5135a3188de6ce0e98f4c799efa76807e19bcb
oskarmast <oskarmast9436@gmail.com>
2022-10-03 13:30:36 +0600

    fix(frontend): add style in homepage

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,18 +1,46 @@
 import React, { useEffect } from "react";
 import { isLogin } from "../utils/auth";
 import { useNavigate } from "react-router-dom";
+import { Box, Card, Typography } from "@mui/material";
 
 const Homepage = () => {
   const navigate = useNavigate();
-
+  const [userName, setUserName] = React.useState("");
   useEffect(() => {
     if (!isLogin()) {
       navigate("/");
     }
+    const employeeHome: any = JSON.parse(localStorage.getItem("user") || "{}");
+    setUserName(employeeHome.name);
   });
   return (
     <div>
-      <h1>Welcome to our website</h1>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Card
+          sx={{
+            height: 600,
+            width: 600,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            margin: 2,
+            bgcolor: "#1c0f67",
+            color: "white",
+            borderRadius: 2,
+            boxShadow: 3,
+          }}
+        >
+          <Typography variant="h4" component="div" sx={{ margin: "auto" }}>
+            Welcome {userName}
+          </Typography>
+        </Card>
+      </Box>
     </div>
   );
 };
