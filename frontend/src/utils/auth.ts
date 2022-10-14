6c68819e858f07361b350e1baff44df06d73059b
oskarmast <oskarmast9436@gmail.com>
2022-10-14 17:24:45 +0600

    fix: jwt expire logout

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,5 +1,12 @@
+import jwt_decode from "jwt-decode";
+
 export const isLogin = () => {
   if (localStorage.getItem("token")) {
+    const user: any = jwt_decode(localStorage.getItem("token") || "");
+    if (user.exp < Date.now() / 1000) {
+      localStorage.removeItem("token");
+      return false;
+    }
     return true;
   }
   //alert("token is not exist");
