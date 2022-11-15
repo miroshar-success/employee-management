fb3cc686d45634cfefa69f624214f7504337813d
oskarmast <oskarmast9436@gmail.com>
2022-11-15 19:39:35 +0600

    feat: project navigation

---
+++ frontend/src/components/Pagination.tsx
@@ -0,0 +1,49 @@
+import { Button, Box } from "@mui/material";
+import React from "react";
+
+const Pagination = ({ setCurrentPages, currentPage }: any) => {
+  const handleNextPage = () => {
+    setCurrentPages((prev: number) => prev + 1);
+  };
+  const handlePrevPage = () => {
+    setCurrentPages((prev: number) => prev - 1);
+  };
+  console.log("currentPage", currentPage);
+  return (
+    <div style={{ width: "100%", padding: 2 }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          width: "95%",
+          justifyContent: "space-between",
+          padding: "10px",
+        }}
+      >
+        <Button
+          variant="contained"
+          onClick={
+            currentPage > 1
+              ? () => setCurrentPages(parseInt(currentPage) - parseInt("1"))
+              : () => setCurrentPages(parseInt(currentPage))
+          }
+        >
+          Previous
+        </Button>
+        <Box
+          sx={{ bgcolor: "green", padding: 2, color: "white", borderRadius: 5 }}
+        >
+          Page No: {currentPage}
+        </Box>
+        <Button
+          variant="contained"
+          onClick={() => setCurrentPages(parseInt(currentPage) + parseInt("1"))}
+        >
+          Next
+        </Button>
+      </Box>
+    </div>
+  );
+};
+
+export default Pagination;
--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -4,6 +4,8 @@ import { isLogin } from "../utils/auth";
 import useFetch from "../customHooks/useFetch";
 
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Pagination from "../components/Pagination";
 
 const Projects = ({
   isprofiles,
@@ -13,18 +15,46 @@ const Projects = ({
   setIsProfiles: any;
 }) => {
   const navigate = useNavigate();
-
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const [response, setResponse] = React.useState([]);
+  const [currentPage, setCurrentPages] = React.useState(1);
+  const [currentLimit, setCurrentLimit] = React.useState(3);
+  console.log("currentPage", typeof currentPage);
   useEffect(() => {
     if (!isLogin()) {
       navigate("/");
     }
-  });
+    getProjectData();
+  }, [currentPage]);
 
-  const fecthProjects = useFetch("http://localhost:5000/api/v1/projects");
-  const { response, error, isLoading } = fecthProjects;
+  const getProjectData = async () => {
+    console.log("getProjectData");
+    try {
+      setLoading(true);
+      const res = await axios(
+        `http://localhost:5000/api/v1/projects?currentPage=${currentPage}&currentLimit=${currentLimit}`
+      );
+      const json = await res.data;
+      const { projects, pages, limit } = json;
+      console.log({ projects, pages, limit });
+      setResponse(projects);
+      setCurrentPages(pages);
+      setCurrentLimit(limit);
+    } catch (error: any) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+  // getProjectData();
+  //const fecthProjects = useFetch("http://localhost:5000/api/v1/projects");
+  //const { response, error, isLoading } = fecthProjects;
   if (error) {
     console.log(error);
   }
+  // console.log("response", fecthProjects);
+  // const { projects, pages, limit }: any = response;
+
   const customData = response;
   console.log("customData", customData);
   // const customData = [
@@ -48,10 +78,16 @@ const Projects = ({
   setIsProfiles(false);
   return (
     <div>
-      {isLoading ? (
+      {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <DataTable customData={customData} isprofiles={isprofiles} />
+        <>
+          <DataTable customData={customData} isprofiles={isprofiles} />
+          <Pagination
+            setCurrentPages={setCurrentPages}
+            currentPage={currentPage}
+          />
+        </>
       )}
     </div>
   );
